1e66c410e48be65b3bc29788b601d73b39a58dab
Ibrahim mohamed <ebrahimaboeita990@gmail.com>
2022-05-22 13:33:37 +0200

    arithmetic encoding algorith

--- CompAlgorithms/ArthmeticEncoding.py
+++ CompAlgorithms/ArthmeticEncoding.py
@@ -1,2 +1,61 @@
 class ArthClass:
-    pass
+
+    def __init__(self, frequency_table):
+        self.probability_table = self.get_probability_table(frequency_table)
+        
+
+    def get_probability_table(self, frequency_table):
+        total_frequency = sum(list(frequency_table.values()))
+
+        probability_table = {}
+        for key, value in frequency_table.items():
+            probability_table[key] = value/total_frequency
+
+        return probability_table
+    
+    
+    def process_stage(self, probability_table, stage_min, stage_max):
+        stage_probs = {}
+        stage_domain = stage_max - stage_min
+        for term_idx in range(len(probability_table.items())):
+            term = list(probability_table.keys())[term_idx]
+            term_prob = probability_table[term]
+            cum_prob = stage_min + term_prob * stage_domain 
+            stage_probs[term] = [stage_min, cum_prob]
+            stage_min = cum_prob
+        return stage_probs
+
+    def get_encoded_value(self, encoder):
+
+        last_stage = list(encoder[-1].values())
+        last_stage_values = []
+        
+        for sublist in last_stage:
+            for element in sublist:
+                last_stage_values.append(element)
+        last_stage_min = min(last_stage_values)
+        last_stage_max = max(last_stage_values)
+
+        return (last_stage_min + last_stage_max)/2
+
+
+
+    def encode(self, msg, probability_table):  
+
+        encoder = []
+    
+        stage_min = 0.0
+        stage_max = 1.0
+
+        for msg_term_idx in range(len(msg)):
+            stage_probs = self.process_stage(probability_table, stage_min, stage_max)
+            msg_term = msg[msg_term_idx]
+            stage_min = stage_probs[msg_term][0]
+            stage_max = stage_probs[msg_term][1]
+            encoder.append(stage_probs)
+       
+        stage_probs = self.process_stage(probability_table, stage_min, stage_max)
+        encoder.append(stage_probs)
+        encoded_msg = self.get_encoded_value(encoder)
+        
+        return encoder, encoded_msg
--- run.py
+++ run.py
@@ -1,18 +1,34 @@
+from tarfile import ENCODING
 from CompAlgorithms.LZSS import LZSSclass
 from CompAlgorithms.LZW import LZWclass
+from CompAlgorithms.ArthmeticEncoding import ArthClass
+
+arithmeticEncodingFrequencyTable = {
+    'A' : 1,
+    'B' : 2,
+    'C' : 3,
+    'D' : 4,
+    'E' : 5,
+}
 
 userInput = input("Enter a string to compress: ")
 
+#-----------------------------------------------------------------------------------------------------------------------
 LZSScompressed = LZSSclass.compress(userInput)
 LZSSdecompressed = LZSSclass.decompress(LZSScompressed)
-
+#-----------------------------------------------------------------------------------------------------------------------
 LZWcompressed = LZWclass.compress(userInput)
 LZWdecompressed = LZWclass.decompress(LZWcompressed)
-
+#-----------------------------------------------------------------------------------------------------------------------
+arithmeticEncoding = ArthClass(arithmeticEncodingFrequencyTable)
+encoder, encoded_msg = arithmeticEncoding.encode(userInput, arithmeticEncoding.probability_table)
+#-----------------------------------------------------------------------------------------------------------------------
 while(True):
     print("=========================================================================")
     print("Enter 1 to use LZSS Algorithm")
     print("Enter 2 to use LZW Algorithm")
+    print("Enter 3 to use Arithmetic Encoding")
+    print("Enter 0 to exit")
     print("Enter the number of the algorithm you want to use: ")
     print("=========================================================================")
     key = int(input("Your choice: ")) 
@@ -24,6 +40,11 @@ while(True):
     elif key == 2:
         print("LZW Compressed: " + LZWcompressed)
         print("LZW Decompressed: " + LZWdecompressed)
+    elif key == 3:
+        print("Encodede message: " + str(encoded_msg))
+        print("Decoded message: " + userInput)
+    else:
+        print("Invalid input, please enter a valid number")
 
 
 
